1acc0a3e68ae04a2d269831aa590bcef7e120d86
Fazulhaq Amin <128462337+Fazulhaq@users.noreply.github.com>
2024-07-13 10:52:31 +0430

    first commit

---
+++ src/test/java/com/mcit/DTOPractice/DtoPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mcit.DTOPractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DtoPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
